3fe1e66cab13d36597b20d1e14b2fa785d2e7831
Dudenko Vadim <dudenko.vadim@gmail.com>
2025-03-24 09:44:19 +0300

    Fix tests

--- test/TaskTest.java
+++ test/TaskTest.java
@@ -17,7 +17,7 @@ class TaskTest {
     @Test
     public void shouldBeEqualById() {
         Task task = new Task("name", "test description", Status.NEW, 30, "17.03.2024 21:00");
-        Task task2 = new Task("name", "test description", Status.NEW, 30, "17.03.2024 21:00");
+        Task task2 = new Task("name", "test description", Status.NEW, 30, "17.03.2024 22:00");
 
         taskManager.addTask(task);
         taskManager.addTask(task2);
@@ -37,7 +37,7 @@ class TaskTest {
         assertNotEquals(epic, epic2);
 
         SubTask subTask = new SubTask("Test sub task", "Test sub task description", Status.NEW, epic.getId(), 30, "17.03.2024 17:00");
-        SubTask subTask2 = new SubTask("Test sub task", "Test sub task description", Status.NEW, epic.getId(), 30, "17.03.2024 17:00");
+        SubTask subTask2 = new SubTask("Test sub task", "Test sub task description", Status.NEW, epic.getId(), 30, "17.03.2024 16:00");
 
         taskManager.addSubTask(subTask);
         taskManager.addSubTask(subTask2);
